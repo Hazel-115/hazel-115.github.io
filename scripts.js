beb318d9bf8fd7c372b4f3e2d09538f83fd620a5
Hazel-115 <edgar.doering115@gmail.com>
2024-10-06 21:59:36 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,21 @@
+
+// Countdown Timer
+const weddingDate = new Date("May 12, 2024 12:00:00").getTime();
+
+const countdownTimer = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown-timer").innerHTML = days + " Tage " + hours + " Stunden "
+        + minutes + " Minuten " + seconds + " Sekunden ";
+
+    if (distance < 0) {
+        clearInterval(countdownTimer);
+        document.getElementById("countdown-timer").innerHTML = "Der große Tag ist da!";
+    }
+}, 1000);
